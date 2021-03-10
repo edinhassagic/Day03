44c69894ef6fa0113f0777acc22afc75da1ace02
edin hasagic <edinhassagic@gmail.com>
2021-03-10 16:31:05 +0100

    updated consoles

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,16 +1,18 @@
 function main(numOne, numTwo) {
+    var numOne;    
+    var numTwo;  
+    var sum = numOne + numTwo;
     
-   
-sum = numOne + numTwo;
-
-     
-     
-     
-     
-return sum;
-}
-     
-console.log(main(15, 8),main(5, 28),main(150, 425));
-     
-
-module.exports = main;
+         
+         
+         
+         
+    return sum;
+    }
+         
+    console.log(main(15, 8));
+    console.log(main(5, 28));
+    console.log(main(150, 425));
+         
+    
+    module.exports = main;
