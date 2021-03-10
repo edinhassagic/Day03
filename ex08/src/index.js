ec87300e378f4f66767034794123b644499edae7
edin hasagic <edinhassagic@gmail.com>
2021-03-10 15:31:23 +0100

    Coughing fixed

---
+++ ex08/src/index.js
@@ -0,0 +1,16 @@
+function main(numOne, numTwo) {
+    
+   
+sum = numOne + numTwo;
+
+     
+     
+     
+     
+return sum;
+}
+     
+console.log(main(15, 8),main(5, 28),main(150, 425));
+     
+
+module.exports = main;
